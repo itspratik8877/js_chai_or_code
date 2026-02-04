170d994191f0b8197694ebafe7cd34ef5aa53f17
pratik gupta <PRATIKGUPTAJI995@GMAIL.COM>
2026-02-04 16:38:00 +0000

    new about array and object

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("hites");
+let a=9;
+let ng=-a;
+//console.log(null==undefined);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==="p");
---
+++ 01_basics/understand_ob_fun_arr.js
@@ -0,0 +1,10 @@
+let arr=[10,199,"sdd","ds"];
+console.log(arr[3]);
+console.log(typeof arr)//object
+let university ={
+    collegename: "TransitionEvent",
+    colllegercolor:"yellow"
+
+}
+console.log(university.collegename);
+console.log(university.colllegercolor);
